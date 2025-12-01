5de85f18031eace4763ef7e68d372b546cc29345
cong.le <cong.le@tcu.edu>
2025-12-01 08:44:45 -0600

    Finished question3

---
+++ question3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int isPrime(int n) {
+    if (n <= 1)
+        return 0;
+
+    for (int i = 2; i * i <= n; i++) {
+        if (n % i == 0)
+            return 0;
+    }
+
+    return 1;
+}
+
+int main() {
+    int n;
+
+    printf("Enter an integer: ");
+    scanf("%d", &n);
+
+    if (isPrime(n))
+        printf("Prime\n");
+    else
+        printf("Not Prime\n");
+
+    return 0;
+}
